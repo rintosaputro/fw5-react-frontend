1704cddb9f4cd3ab5cb47c9a64fe038d7b130edc
rintosaputro <saputrorinto@gmail.com>
2022-02-24 07:17:02 +0700

    feat: add vehicle detail page

--- src/pages/VehicleDetail.js
+++ src/pages/VehicleDetail.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import '../assets/css/vehicle-detail.css'
+import {BiMinus, BiPlus} from 'react-icons/bi'
+import {GrFormPrevious, GrFormNext} from 'react-icons/gr'
 
 export default class VehicleDetail extends Component {
   render() {
@@ -9,19 +12,21 @@ export default class VehicleDetail extends Component {
           <div class="row pt-5 detail-vehicle">
             <div class="col-12 col-lg-6 img-section">
               <div class="cover-image overflow-hidden">
-                <img src="/assets/images/fixie-detail.png" alt="fixie" />
+                <img src="../assets/images/fixie-detail.png" alt="fixie" />
               </div>
               <div class="row carousel d-flex align-items-center mt-4">
                 <button class="col-1 btn" aria-label="previous button">
+                  <GrFormPrevious className='prev' />
                   <i class="fa-solid fa-angle-left prev"></i>
                 </button>
                 <div class="col-5 overflow-hidden rounded">
-                  <img src="/assets/images/fixie-detail.png" alt="fixie" class="rounded" />
+                  <img src="../assets/images/fixie-detail.png" alt="fixie" class="rounded" />
                 </div>
                 <div class="col-5 overflow-hidden rounded">
-                  <img src="/assets/images/fixie-detail.png" alt="fixie" class="rounded" />
+                  <img src="../assets/images/fixie-detail.png" alt="fixie" class="rounded" />
                 </div>
                 <button class="col-1 btn" aria-label="next button">
+                  <GrFormNext className='next' />
                   <i class="fa-solid fa-angle-right next"></i>
                 </button>
               </div>
@@ -49,13 +54,30 @@ export default class VehicleDetail extends Component {
               </div>
               <div class="my-auto">
                 <div class="total-day d-flex flex-row justify-content-between align-items-center">
-                  <button class="btn" aria-label="button plus"><i class="fa-solid fa-plus plus"></i></button>
+                  <button class="btn plus" aria-label="button plus"><BiPlus className=''/></button>
                   <div class="count">2</div>
-                  <button class="btn" aria-label="button minus"><i class="fa-solid fa-minus minus"></i></button>
+                  <button class="btn minus" aria-label="button minus"><BiMinus className=''/></button>
                 </div>
               </div>
             </div>
           </div>
+        </section>
+
+        <section class="container form-section mt-5">
+          <form class="row">
+            <div class="col-12 col-md">
+              <a href="#" class="btn btn-black">Chat Admin</a>
+            </div>
+            <div class="col-12 col-md text-center btn-reservation">
+              <a href="/reservation.html" class="btn btn-green">Reservation</a>
+            </div>
+            <div class="col-12 col-md-3 text-end">
+              <button class="btn btn-black">
+                <i class="fa-solid fa-heart"></i>
+                Like
+              </button>
+            </div>
+          </form>
         </section>
       </div>
     )
